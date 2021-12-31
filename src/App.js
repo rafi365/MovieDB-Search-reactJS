2e9fad80b991045d77a3c70cd11a873c050b2a63
rafi365 <74255365+rafi365@users.noreply.github.com>
2021-12-31 10:11:33 +0700

    more css and text

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends React.Component {
         </div>
         <div className='split right'>
           <MovieDetails loading={this.state.detailIsLoading} result={this.state.moviedetailsresult}/>
-          <p className='credits'>App created with <a href='https://reactjs.org/'>ReactJS</a></p>
+          <p className='credits'>App created with <a target='_blank' href='https://reactjs.org/'>ReactJS</a> using <a target='_blank' href='https://rapidapi.com/hmerritt/api/imdb-internet-movie-database-unofficial/'>IMDB (unofficial) API</a></p>
         </div>
       </div>
     );
